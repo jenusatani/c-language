f6ea28c2e30c53c1dd4c6513695f1c35feb6983b
jenusatani <162433577+jenusatani@users.noreply.github.com>
2024-03-31 21:12:13 +0530

    wt 4
    wt 4

---
+++ Weekly task - 4/web.js
@@ -0,0 +1,99 @@
+$(document).ready(function () {
+    $('.a1').mouseenter(function () {
+        $('.a1').css('color', 'aquamarine')
+        $('.a1').mouseleave(function () {
+            $('.a1').css('color', 'white')
+
+        })
+    })
+
+    $('.a2').mouseenter(function () {
+        $('.a2').css('color', 'aquamarine')
+        $('.a2').mouseleave(function () {
+            $('.a2').css('color', 'white')
+
+        })
+    })
+
+    $('.a3').mouseenter(function () {
+        $('.a3').css('color', 'aquamarine')
+        $('.a3').mouseleave(function () {
+            $('.a3').css('color', 'white')
+
+        })
+    })
+
+    $('.a4').mouseenter(function () {
+        $('.a4').css('color', 'aquamarine')
+        $('.a4').mouseleave(function () {
+            $('.a4').css('color', 'white')
+
+        })
+    })
+
+    $('.a5').mouseenter(function () {
+        $('.a5').css('color', 'aquamarine')
+        $('.a5').mouseleave(function () {
+            $('.a5').css('color', 'white')
+
+        })
+    })
+
+    $('.a6').mouseenter(function () {
+        $('.a6').css('color', 'aquamarine')
+        $('.a6').mouseleave(function () {
+            $('.a6').css('color', 'white')
+
+        })
+    })
+
+    $('.b1').mouseenter(function () {
+        $('.b1').css('color', 'cadetblue')
+        $('.b1').mouseleave(function () {
+            $('.b1').css('color', 'white')
+
+        })
+    })
+
+    $('.b2').mouseenter(function () {
+        $('.b2').css('color', 'cadetblue')
+        $('.b2').mouseleave(function () {
+            $('.b2').css('color', 'white')
+
+        })
+    })
+
+    $('.d1').mouseenter(function () {
+        $('.d1').css('color', 'chartreuse')
+        $('.d1').mouseleave(function () {
+            $('.d1').css('color', '')
+
+        })
+    })
+
+    $('.d2').mouseenter(function () {
+        $('.d2').css('color', 'chartreuse')
+        $('.d2').mouseleave(function () {
+            $('.d2').css('color', '')
+
+        })
+    })
+
+    $('.b2').mouseenter(function () {
+        $('.b2').css('color', 'cadetblue')
+        $('.b2').mouseleave(function () {
+            $('.b2').css('color', 'white')
+
+        })
+    })
+
+    $('.ff1').mouseenter(function () {
+        $('.ff1').css('color', 'cadetblue')
+        $('.ff1').mouseleave(function () {
+            $('.ff1').css('color', 'white')
+
+        })
+    })
+
+    
+})
