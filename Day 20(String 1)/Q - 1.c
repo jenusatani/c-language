574b165eadd05ac129d7dfa2e7e59b15cd5ec61c
jenusatani <162433577+jenusatani@users.noreply.github.com>
2024-04-03 15:43:37 +0530

    day 20
    day 20

---
+++ Day 20(String 1)/Q - 1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main()
+
+
+{
+
+    char val[] = "hello world";
+
+    for (int i = 0; val[i] !=NULL; i++)
+    {
+        if (val[i] >= 'a' && val[i]  <= 'z')
+
+        {
+
+            val[i] -= 32;
+        }
+
+        printf("%c",val[i]);
+        
+    }
+    
+}
